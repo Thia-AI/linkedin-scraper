ff56e6306c97c6e68ae676ea197fa8dd61108f04
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2022-11-28 20:44:13 -0500

    Lowered min time for random sleep from 10->5 seconds

--- linkedin_scraper/helper.py
+++ linkedin_scraper/helper.py
@@ -3,6 +3,6 @@ from time import sleep
 from numpy import random
 
 
-def sleep_for_a_random_time(low=10, high=20):
+def sleep_for_a_random_time(low=5, high=20):
     sleep_time = random.uniform(low, high)
     sleep(sleep_time)
